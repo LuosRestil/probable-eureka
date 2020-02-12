6249a9be75eaadabc5bcd03eb64e6ae1d34a1eea
LuosRestil <metaltasteslikechicken@gmail.com>
2020-02-12 16:28:56 -0600

    Complete but untested

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+app.post("/", (req, res) => {
+  var mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
